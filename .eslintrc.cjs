1ab20dc998b8beda747b0d759b950a9321c276ff
Lucas <29964166+Lucasnribeiro@users.noreply.github.com>
2023-06-21 18:42:04 -0300

    disable "@typescript-eslint/no-misused-promises"

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -29,7 +29,14 @@ const config = {
       },
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-    "@typescript-eslint/no-misused-promises": "off"
+    "@typescript-eslint/no-misused-promises": [
+      "error",
+      {
+        "checksConditionals": false,
+        "checksVoidReturn": false
+      }
+    ]
+  
   },
 };
 
